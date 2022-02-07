e86eebdc931ccf0535bbc8a291dd7286e8a951e7
Chris Martin <chris.martin@oforce.com>
2022-02-07 00:58:04 -0700

    move config to config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,5 @@
 import Config
 
+config :cqrs_tools, :context_shipper, Cqrs.Absinthe.Test.DispatchContextShipper
+
 config :logger, :console, format: "[$level] $message\n", level: :warning
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,3 @@
 Cqrs.Repo.start_link([])
-Application.put_env(:cqrs_tools, :context_shipper, Cqrs.Absinthe.Test.DispatchContextShipper)
+
 ExUnit.start()
